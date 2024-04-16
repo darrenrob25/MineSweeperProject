b146c76066a13cdca1b2399d5244a7980d1d65d1
Darren <darrenrobinsonpt@gmail.com>
2024-04-16 21:00:38 +0100

    Added JS to start a new game when the window loads, started to build startGame function.

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,3 +9,11 @@ var tilesClicked = 0; // total number of tiles clicked.
 var flagEnabled = false; //This is used to dictate whether we have clicked the flag button and whether its activated.
 
 var gameOver = false; //Used to decide whether game is over yet.
+
+window.onload = function() { //Will initially start the game when we load the webpage.
+    startGame();
+}
+
+function startGame() {
+    
+}
