9cd27977e531f7aea24de7d52aaf6eede2f92db3
Darren <darrenrobinsonpt@gmail.com>
2024-04-16 20:58:30 +0100

    Initial javascript added, added variables for the base game, such as the board, the number of mines, created array for their location etc.

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+var board = [];
+var rows = 8;
+var columns = 8;
+
+let minesCount = 5; //Total number of mines in game.
+var minesLocation = []; //grid co-ordinates of mines.
+
+var tilesClicked = 0; // total number of tiles clicked.
+var flagEnabled = false; //This is used to dictate whether we have clicked the flag button and whether its activated.
+
+var gameOver = false; //Used to decide whether game is over yet.
